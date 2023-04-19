dfce90eb39dbf12b08b619c415679f83f1b0c0e1
giric <girichev2013@yandex.ru>
2023-04-19 20:37:23 +0500

    19.04.2023 - RequestControllerPrivate+

--- ewm-main-service/src/main/java/ru/practicum/events/request/controller/RequestControllerPrivate.java
+++ ewm-main-service/src/main/java/ru/practicum/events/request/controller/RequestControllerPrivate.java
@@ -2,18 +2,48 @@ package ru.practicum.events.request.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.events.request.dto.ParticipationRequestDto;
 import ru.practicum.events.request.service.RequestServicePrivate;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users/{userId}/requests")
 @Slf4j
 public class RequestControllerPrivate {
     private final RequestServicePrivate requestServicePrivate;
+
     @Autowired
     public RequestControllerPrivate(RequestServicePrivate requestServicePrivate) {
         this.requestServicePrivate = requestServicePrivate;
+    }
+    /**
+     * Информация о заявках текущего пользователя на участие в чужих событиях
+     */
+    @GetMapping
+    List<ParticipationRequestDto> getAllRequestsUserById(@NotNull @PathVariable Long userId) {
+        return requestServicePrivate.getAllRequestsUserById(userId);
+    }
+    /**
+     * Добавление запроса от текущего пользователя на участие в событии
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    ParticipationRequestDto addRequestEventById(@NotNull @PathVariable Long userId,
+                                                @NotNull @RequestParam Long eventId) {
+        return requestServicePrivate.addRequestEventById(userId, eventId);
+    }
+
+    /**
+     * Отмена своего запроса на участие в событии
+     */
+    @PatchMapping("/{requestId}/cancel")
+    ParticipationRequestDto updateRequestById(@NotNull @PathVariable Long userId,
+                                              @NotNull @PathVariable Long requestId){
+        return requestServicePrivate.updateRequestById(userId, requestId);
     }
 
 
