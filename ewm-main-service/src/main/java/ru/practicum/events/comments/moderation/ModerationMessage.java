134afce59c71a93f3d7cc1ac1294dd0567fcb172
giric <girichev2013@yandex.ru>
2023-05-08 11:21:32 +0500

    07.05.2023 - поправил класс модерации сообщений

--- ewm-main-service/src/main/java/ru/practicum/events/comments/moderation/ModerationMessage.java
+++ ewm-main-service/src/main/java/ru/practicum/events/comments/moderation/ModerationMessage.java
@@ -1,11 +1,11 @@
 package ru.practicum.events.comments.moderation;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.practicum.exception.BadRequestException;
 import ru.practicum.exception.ResourceNotFoundException;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,20 +15,14 @@ import java.util.stream.Stream;
 
 @Service
 public class ModerationMessage {
-    @Value("${moderation.words.file.path}")
-    private String wordsFilePath;
-
     public void moderationMessage(String text) {
-        try {
-            String url = "src/main/java/ru/practicum/events/comments/moderation/words.txt";
-            Stream<String> lines = Files.lines((Paths.get(url)), Charset.defaultCharset());
-            List<String> forbiddenWords = lines.filter(s -> !s.isEmpty()).collect(Collectors.toList());
+        try (Stream<String> lines = Files.lines(Paths.get(getClass().getClassLoader().getResource("words.txt").toURI()), Charset.defaultCharset())) {
             List<String> newText = Stream.of(text.split(" ")).map(String::toLowerCase).collect(Collectors.toList());
-            newText.retainAll(forbiddenWords);
+            newText.retainAll(lines.collect(Collectors.toList()));
             if (!newText.isEmpty()) {
                 throw new BadRequestException("Комментарий является недопустимым и отклонен");
             }
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             throw new ResourceNotFoundException("Ошибка чтения библиотеки для модерации контента");
         }
     }
